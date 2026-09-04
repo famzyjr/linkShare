d6410cd1d1e6b2c729fe927d41244a439a54b194
famzyjr <famuyiwaahmed132@gmail.com>
2026-09-04 02:35:57 +0100

    initialized  react hooks form

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,11 +1,11 @@
+
+
 import DeafultLogo from "../../components/DeafultLogo"
 import Text from "../../components/Text"
+
 const Login = () => {
 
-  const handleSubmit=(e:React.ChangeEvent<HTMLInputElement>)=>{
-   e.preventDefault();
-   console.log('s')
-  }
+
 
   return (
     <>
@@ -17,32 +17,44 @@ const Login = () => {
           </div>
 
           <div className="login_con bg-[#FFFFFF] w-full h-full rounded-xl p-5   flex flex-col gap-8 ">
-           <div className="flex flex-col m-5 gap-10">
-             <div>
-              <h2 className="font-bold text-[32px] text-[#333333]">Login</h2>
-              <p className="font-normal text-[16px] text-[#737373]">Add your details below to get back into the app</p>
+            <div className="flex flex-col m-5 gap-10">
+              <div>
+                <h2 className="font-bold text-[32px] text-[#333333]">Login</h2>
+                <p className="font-normal text-[16px] text-[#737373]">Add your details below to get back into the app</p>
               </div>
 
-            <form  onSubmit={handleSubmit}>
+              <form  >
 
                 <div className="flex flex-col gap-6">
-              <div className="flex flex-col">
-                <label  htmlFor="email">Email</label>
-                <input type="text" placeholder="e.g. alex@email.com" id="email"/>
-              </div>
+                  <div className="flex flex-col">
+                    <label htmlFor="email">Email</label>
+                    <input
 
-              <div className="flex flex-col">
-                <label htmlFor="password">Password</label>
-                <input type="text" id="password" placeholder="Enter Your password" />
-              </div>
-              <button className="bg-[#633CFF] rounded-lg w-99 h-11.5 text-[#FFFFFF]">Login</button>
-              <div className="text-center">
-           <span className="text-[16px] font-normal">Don’t have an account? <button className=" text-[#633CFF]">Create account</button></span>
-              </div>
+                      type="text"
+                      placeholder="e.g. alex@email.com"
+                      id="email"
 
+                    />
+
+                  </div>
+
+                  <div className="flex flex-col">
+                    <label htmlFor="password">Password</label>
+                    <input type="text"
+
+                      id="password"
+                      placeholder="Enter Your password"
+
+                    />
+                  </div>
+                  <button className="bg-[#633CFF] rounded-lg w-99 h-11.5 text-[#FFFFFF]">Login</button>
+                  <div className="text-center">
+                    <span className="text-[16px] font-normal">Don’t have an account? <button className=" text-[#633CFF]">Create account</button></span>
+                  </div>
+
+                </div>
+              </form>
             </div>
-            </form>
-           </div>
           </div>
 
 
